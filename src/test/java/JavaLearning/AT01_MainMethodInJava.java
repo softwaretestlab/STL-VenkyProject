cf384af69c9959c7299d3c70416bba503fe985f4
softwaretestlab <softwaretestlab@hotmail.com>
2022-12-17 08:03:53 +1100

    start

---
+++ src/test/java/JavaLearning/AT01_MainMethodInJava.java
@@ -0,0 +1,13 @@
+//package
+package JavaLearning;
+//class
+public class AT01_MainMethodInJava {
+    //method-->main method
+
+    public static void main(String[] args){
+        //we write code
+        System.out.println("This is my First Java class");
+
+
+    }
+}
