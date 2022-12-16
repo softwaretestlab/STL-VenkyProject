579fd129d621c33f2f16e69a9c81bf7f9a685941
softwaretestlab <softwaretestlab@hotmail.com>
2022-12-17 08:28:02 +1100

    junits

--- src/test/java/JavaLearning/AT02_JunitMethods.java
+++ src/test/java/JavaLearning/AT02_JunitMethods.java
@@ -3,7 +3,7 @@ package JavaLearning;
 import org.junit.Test;
 
 public class AT02_JunitMethods {
-    //we can have multiple methods
+    //we can have multiple of these below methods
     @Test
     public void test01(){
         System.out.println("This is method 1");
